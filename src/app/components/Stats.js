546217787c8d9f3424b7715adfe5a8dcd75e7630
kiccek <kiccek@gmail.com>
2018-06-23 15:45:37 +0200

    changed from ejs to reactJS

---
+++ src/app/components/Stats.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ChartistGraph from 'react-chartist';
+
+export class Stats extends React.Component {
+    state = {
+        stats: [],
+    };
+
+    componentDidMount(){
+        $.get('http://localhost:8000/stats').then((res) => this.setState({ stats: res }));
+    }
+
+    render() {
+        const all_rides = ((this.state.stats || {}).all_ride_totals || {}).count;
+        const all_runs = ((this.state.stats || {}).all_run_totals || {}).count;
+        const all_swims = ((this.state.stats || {}).all_swim_totals || {}).count;
+        const total = (all_runs + all_swims + all_rides) / 100;
+
+        const data = {
+            labels: ['Runs ' + Math.round(all_runs / total) + '%', 'Swims ' + Math.round(all_swims / total) + '%', 'Rides ' + Math.round(all_rides / total) + '%'],
+            series: [all_runs, all_swims, all_rides]
+        };
+
+        const options = {
+            labelInterpolationFnc: function (value) {
+                return value[0]
+            }
+        };
+
+        const responsiveOptions = [
+            ['screen and (min-width: 640px)', {
+                chartPadding: 30,
+                labelOffset: 100,
+                labelDirection: 'explode',
+                labelInterpolationFnc: function (value) {
+                    return value;
+                }
+            }],
+            ['screen and (min-width: 1024px)', {
+                labelOffset: 80,
+                chartPadding: 20
+            }]
+        ];
+
+        const type='Pie';
+
+        return (
+            <div>
+                <div>
+                    <ChartistGraph
+                        data={data}
+                        options={options}
+                        responsiveOptions={responsiveOptions}
+                        type={type}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
